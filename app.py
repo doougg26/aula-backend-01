d4e8b8c320fb16c65124b0df86d4209bea136fb3
Douglas C. Rodrigues <dcorrea566@gmail.com>
2025-03-14 10:46:23 -0300

    fix: sql injection and update database

--- app.py
+++ app.py
@@ -31,12 +31,11 @@ def doar():
     autor = dados.get('autor')
     imagem_url = dados.get('imagem_url')
 
-    if not titulo or not categoria or not autor or not imagem_url:
+    if not all([titulo, categoria, autor, imagem_url]):
         return jsonify({'erro':'todos os campos são obrigatórios'}), 400
     
     with sqlite3.connect('database.db') as conn:
-        conn.execute(f"""  INSERT INTO livros (titulo, categoria, autor, imagem_url) values 
-                           ('{titulo},{categoria},{autor},{imagem_url}') """)
+        conn.execute(f"""  INSERT INTO livros (titulo, categoria, autor, imagem_url) values (?,?,?,?)""",(titulo,categoria,autor,imagem_url))
         
         conn.commit()
 
